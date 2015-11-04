0ac7c85d8845ea6e68b9971e78fbbb8065e683a7
Charles Buhler <charlesbuhler@gmail.com>
2015-11-04 11:27:15 -0800

    Initial Commit

---
+++ CedarSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CedarSample
+//
+//  Created by Charles Buhler on 11/4/15.
+//  Copyright © 2015 Charles Buhler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CedarSampleTests/CedarSampleTests.m
@@ -0,0 +1,39 @@
+//
+//  CedarSampleTests.m
+//  CedarSampleTests
+//
+//  Created by Charles Buhler on 11/4/15.
+//  Copyright © 2015 Charles Buhler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CedarSampleTests : XCTestCase
+
+@end
+
+@implementation CedarSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
